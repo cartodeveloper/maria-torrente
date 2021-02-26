a07b7a263ef5395a72abe2952d63a4b79a2ec17b
María Torrente <caritotorrente@gmail.com>
2021-02-26 18:50:07 -0500

    Adding TasteBuddy and SP Capstones

--- script.js
+++ script.js
@@ -46,25 +46,55 @@ function plusSlides2(n) {
   showSlides2((slideIndex2 += n));
 }
 
-function currentSlide(n) {
+function currentSlide2(n) {
   showSlides2((slideIndex2 = n));
 }
 function showSlides2(slide2) {
   let i;
-  let slides = document.getElementsByClassName("mySlides2");
-  let dots = document.getElementsByClassName("dot2");
-  if (slide2 > slides.length) {
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("dots2");
+  if (slide2 > slides2.length) {
     slideIndex2 = 1;
   }
   if (slide2 < 1) {
     slideIndex2 = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
   }
-  slides[slideIndex2 - 1].style.display = "block";
-  dots[slideIndex2 - 1].className += " active";
+  slides2[slideIndex2 - 1].style.display = "block";
+  dots2[slideIndex2 - 1].className += " active";
+}
+/* Slider3*/
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function plusSlides3(n) {
+  showSlides3((slideIndex3 += n));
+}
+
+function currentSlide3(n) {
+  showSlides3((slideIndex3 = n));
+}
+function showSlides3(slide3) {
+  let i;
+  let slides3 = document.getElementsByClassName("mySlides3");
+  let dots3 = document.getElementsByClassName("dots3");
+  if (slide3 > slides3.length) {
+    slideIndex3 = 1;
+  }
+  if (slide3 < 1) {
+    slideIndex3 = slides3.length;
+  }
+  for (i = 0; i < slides3.length; i++) {
+    slides3[i].style.display = "none";
+  }
+  for (i = 0; i < dots3.length; i++) {
+    dots3[i].className = dots3[i].className.replace(" active", "");
+  }
+  slides3[slideIndex3 - 1].style.display = "block";
+  dots3[slideIndex3 - 1].className += " active";
 }
